d797b1562fac74202bc88cf4c6cb2a76149978ff
Omar Qattam <omarqattam99@gmail.com>
2022-09-12 23:22:04 +0300

    whiteboard

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 const {Sequelize, DataTypes} = require('sequelize');
 const Post = require('./post.model');
 
-const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://omar:0000@localhost:5432/postgres"
+const POSTGRES_URL = process.env.DATABASE_URL || 'postgresql://omar:0000@localhost:5432/postgres'
 
 let sequelize = new Sequelize(POSTGRES_URL);
 
