0ed9c95e053b713f00b4abe897ff845ea44464e9
Omar Qattam <65464415+OmarQatt@users.noreply.github.com>
2022-09-13 15:09:35 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,9 +3,17 @@
 const {Sequelize, DataTypes} = require('sequelize');
 const Post = require('./post.model');
 
-const POSTGRES_URL = 'postgres://hkdrqlxsebrdww:63e770231804f17f4105a8cb9cedf1663b16b78d99411a31a495a82871d6ace1@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/d373aa7aaq3lqs' || 'postgresql://omar:0000@localhost:5432/postgres'
+const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://omar:0000@localhost:5432/postgres"
 
-let sequelize = new Sequelize(POSTGRES_URL);
+const sequelizeOptions = {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+}
+let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
 module.exports = {
     db: sequelize,
