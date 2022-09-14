5c3ec948342899bd593d0ff3baddc10aea9eafb2
Omar Qattam <omarqattam99@gmail.com>
2022-09-14 14:23:49 +0300

    newTest

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const Post = require('./post.model');
 const POSTGRES_URL = process.env.DATABASE_URL || 'postgresql://omar:0000@localhost:5432/postgres'
 
 const sequelizeOption = {
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   };
 let sequelize = new Sequelize(POSTGRES_URL, sequelizeOption);
 
