3545a88b75fc6ba6c43e7dfe9dcc02ae94b01642
Omar Qattam <omarqattam99@gmail.com>
2022-09-24 19:59:14 +0000

    auth

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,7 +6,7 @@ const userAuth = require('../middlewares/userAuth');
 const router = require('express').Router();
 
 router.post('/login', login);
-router.post('/signup', userAuth, signup)
+router.post('/signup', userAuth.saveUser, signup)
 router.get('/users',  allUser)
 
 module.exports = router;
